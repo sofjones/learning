540a11f33b348ca3820a71688efe5442b2c43105
unknown <sofiaj611@gmail.com>
2024-05-17 20:29:57 -0600

    make cmd class

--- base.py
+++ base.py
@@ -1,15 +1,39 @@
 import os
 import subprocess
+
+class Command:
+    def __init__(self, cmd, flags=[],target=""):
+      self.cmd = cmd
+      self.flags = flags  
+      self.target = target
+
+    def run_command(self):
+        if not self.target:
+            print(self.cmd)
+            result = subprocess.run(self.cmd, shell=True, capture_output=True, text=True)
+        else:
+            full_cmd = self.cmd + " " + self.target
+            result = subprocess.run(full_cmd, shell=True, capture_output=True, text=True)
+        if result.returncode == 0:
+            output = result.stdout.strip()
+            print("Command output:", output)
+        else:
+            print("Command failed with error:", result.stderr)
 print("you are: ")
 os.system("whoami")
 
-def run_command(command):
-    result = subprocess.run(command, shell=True, capture_output=True, text=True)
-    if result.returncode == 0:
-        output = result.stdout.strip()
-        print("Command output:", output)
-    else:
-        print("Command failed with error:", result.stderr)
 
 
-run_command("dir")
+url = input("please input url")
+opts = "would you like to ping google?"
+ping = Command("ping",[],url)
+ping.run_command()
+
+
+ip_addr = "ipconfig | findstr IPv4"
+nslookup = "nslookup"
+trace = "tracert -d " + url
+mac_addr = "getmac"
+tasks = "tasklist | findstr chrome"
+arp = "arp -a"
+host = "hostname"
